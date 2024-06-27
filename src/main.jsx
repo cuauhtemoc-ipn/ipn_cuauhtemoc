1ff0a65c61d0a98ed75a6197eaba6f33d8952868
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-06-27 13:58:24 -0600

    Inclusion of scss to modify default bootstrap parameters

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 // Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/styles/main.scss'
 // Bootstrap Bundle JS
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
