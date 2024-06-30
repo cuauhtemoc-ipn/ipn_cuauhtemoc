a9c3aac45669caa9b53d4574ee025295a0333a64
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-06-30 08:12:57 -0600

    Enabled toggle button in navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,47 +9,71 @@ const Header = () => {
   }
 
   return (
-    <nav className='bg-dark py-1 px-5 container-fluid d-flex justify-content-between align-items-center'>
-      <NavLink to='/' className=''>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-1 px-5 container-fluid d-flex justify-content-between align-items-center'>
+      <NavLink to='/' className='my-2'>
         <img src={logo} alt='IPN Cuauhtemoc Logo' />
       </NavLink>
-      <ul className='list-inline d-flex m-0'>
-        <li className='m-4'>
-          <NavLink to='/' className={({ isActive }) => linkIsActive(isActive)}>
-            Inicio
-          </NavLink>
-        </li>
-        <li className='m-4'>
-          <NavLink
-            to='/aboutUs'
-            className={({ isActive }) => linkIsActive(isActive)}
-          >
-            Acerca de
-          </NavLink>
-        </li>
 
-        <li className='m-4'>
-          <NavLink
-            to='/projects'
-            className={({ isActive }) => linkIsActive(isActive)}
-          >
-            Proyectos
-          </NavLink>
-        </li>
-        <li className='m-4'>
-          <NavLink
-            to='/join'
-            className={({ isActive }) => linkIsActive(isActive)}
-          >
-            Involúcrate
-          </NavLink>
-        </li>
-        <li className='m-4'>
-          <NavLink to='/contact' className={({ isActive }) => linkIsActive(isActive)}>
-            Contacto
-          </NavLink>
-        </li>
-      </ul>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#main-nav'
+        aria-controls='main-nav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon navbar-toggler-icon-bg-primary navbar-toggler-border-color-light' />
+      </button>
+
+      <div
+        className='collapse navbar-collapse justify-content-end align-center w-auto'
+        id='main-nav'
+      >
+        <ul className='list-inline d-md-flex m-0'>
+          <li className='m-4'>
+            <NavLink
+              to='/'
+              className={({ isActive }) => linkIsActive(isActive)}
+            >
+              Inicio
+            </NavLink>
+          </li>
+          <li className='m-4'>
+            <NavLink
+              to='/aboutUs'
+              className={({ isActive }) => linkIsActive(isActive)}
+            >
+              Acerca de
+            </NavLink>
+          </li>
+
+          <li className='m-4'>
+            <NavLink
+              to='/projects'
+              className={({ isActive }) => linkIsActive(isActive)}
+            >
+              Proyectos
+            </NavLink>
+          </li>
+          <li className='m-4'>
+            <NavLink
+              to='/join'
+              className={({ isActive }) => linkIsActive(isActive)}
+            >
+              Involúcrate
+            </NavLink>
+          </li>
+          <li className='m-4'>
+            <NavLink
+              to='/contact'
+              className={({ isActive }) => linkIsActive(isActive)}
+            >
+              Contacto
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
