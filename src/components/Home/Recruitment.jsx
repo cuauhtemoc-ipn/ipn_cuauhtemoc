dc7a285972210434144af89890eff02e3e58ef1c
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-07-15 14:08:07 -0600

    Updated layout of recruitment section

--- src/components/Home/Recruitment.jsx
+++ src/components/Home/Recruitment.jsx
@@ -4,7 +4,7 @@ const Recruitment = () => {
   return (
     <div className='container-xxl h-full py-4 align-content-center'>
       <div className='row'>
-        <aside className='col-12 col-lg-6'>
+        <aside className='col-12 col-md-6'>
           <h2 className='display-3 text-light fw-semibold mb-5'>
             ¡Forma parte del equipo!
           </h2>
@@ -17,23 +17,24 @@ const Recruitment = () => {
             Formarás parte de un ambiente colaborativo donde el aprendizaje y el
             crecimiento profesional son continuos.{' '}
           </p>
-          <p className='text-light'>
+          <p className='text-light d-none d-xl-block'>
             No solo buscamos conocimiento técnico, sino también un espíritu de
             equipo y una actitud proactiva. Queremos personas que estén
             dispuestas a aprender, compartir sus ideas y contribuir al éxito
             colectivo. Si te identificas con estos valores y estás listo para
             enfrentar nuevos desafíos, no dudes en postularte.
           </p>
-          <p className='text-light lead '>
+          <p className='text-light lead d-block d-md-none d-lg-block'>
             Únete a nosotros y forma parte de la próxima generación de líderes
             en ingeniería aeroespacial!
           </p>
-
-          <button className='news__button btn btn-outline-light btn-lg px-5 m-3 justify-self-center'>
-            Ver la ultima convocatoria ➔
-          </button>
+          <div className='container-full d-flex justify-content-center'>
+            <button className='news__button btn btn-outline-light btn-lg px-5 m-3'>
+              Ver la ultima convocatoria ➔
+            </button>
+          </div>
         </aside>
-        <img src={image} alt='' className='col-6' />
+        <img src={image} alt='' className='col-12 col-md-6' />
       </div>
     </div>
   )
