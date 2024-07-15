4266907c8eb22f6444e00ea36b2534373a2a1fda
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-07-15 13:50:39 -0600

    Created recruitment section in home page

---
+++ src/components/Home/Recruitment.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import image from '@/assets/TeamFlag.png'
+const Recruitment = () => {
+  return (
+    <div className='container-xxl h-full py-4 align-content-center'>
+      <div className='row'>
+        <aside className='col-12 col-lg-6'>
+          <h2 className='display-3 text-light fw-semibold mb-5'>
+            ¡Forma parte del equipo!
+          </h2>
+          <p className='text-light'>
+            Si tienes pasión por la ingeniería aeroespacial y deseas formar
+            parte de un equipo dinámico y comprometido con la excelencia, ¡te
+            invitamos a participar en nuestro proceso de reclutamiento anual! Al
+            unirte a nosotros, tendrás la oportunidad de trabajar en proyectos
+            innovadores que desafían los límites de la tecnología aeroespacial.
+            Formarás parte de un ambiente colaborativo donde el aprendizaje y el
+            crecimiento profesional son continuos.{' '}
+          </p>
+          <p className='text-light'>
+            No solo buscamos conocimiento técnico, sino también un espíritu de
+            equipo y una actitud proactiva. Queremos personas que estén
+            dispuestas a aprender, compartir sus ideas y contribuir al éxito
+            colectivo. Si te identificas con estos valores y estás listo para
+            enfrentar nuevos desafíos, no dudes en postularte.
+          </p>
+          <p className='text-light lead '>
+            Únete a nosotros y forma parte de la próxima generación de líderes
+            en ingeniería aeroespacial!
+          </p>
+
+          <button className='news__button btn btn-outline-light btn-lg px-5 m-3 justify-self-center'>
+            Ver la ultima convocatoria ➔
+          </button>
+        </aside>
+        <img src={image} alt='' className='col-6' />
+      </div>
+    </div>
+  )
+}
+
+export default Recruitment
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Descrpt from '../components/Home/Description'
 import News from '@/components/Home/News'
 import Proyect from '../components/Home/Projects'
+import Recruitment from '../components/Home/Recruitment'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <News />
       {/* <hr className='border border-light border-1 opacity-100' /> */}
       <Proyect />
+      <Recruitment />
     </div>
   )
 }
