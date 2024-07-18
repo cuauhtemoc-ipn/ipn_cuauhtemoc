28bebae9e462e66d72ca6c397021bd9222294f55
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-07-18 17:17:54 -0600

    Adjusted news card for consistent size and image aspect-ratio

--- src/components/Home/NewsCard.jsx
+++ src/components/Home/NewsCard.jsx
@@ -3,15 +3,22 @@ import React from 'react'
 
 const NewsCard = ({ data }) => {
   return (
-    <div className='col-10 col-md-6 col-lg-4 news my-4 px-3 shadow'>
+    <div className='col-10 col-md-6 col-lg-4 news my-4 px-3 shadow h-100'>
       <aside className='m-2 bg-light text-dark text-center p-1 news__date'>
         <div className='fs-6 mt-01'>2024</div>
         <div className='fw-semibold fs-1 mt-01'>12</div>
         <div className='fs-7 mt-03'>Septiembre</div>
       </aside>
-      <img src={data.src} alt='News photo' className='w-100 news__image' />
-      <div className='bg-light text-dark p-3  flex-grow-1'>
-        <div>
+      <div className='news__image ratio ratio-1x1'>
+        <img
+          src={data.src}
+          alt='News photo'
+          className='w-100 object-fit-cover'
+        />
+      </div>
+
+      <div className='bg-light text-dark p-3 ratio ratio-21x9 ratio-md-16x9 ratio-lg-21x9 overflow-auto'>
+        <div className='p-2'>
           {data.text.length > 255 ? data.text.slice(0, 255) + '...' : data.text}
         </div>
       </div>
