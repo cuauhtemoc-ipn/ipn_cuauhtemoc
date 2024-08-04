856b7f0ee2ac1444f67f0ca42e96d60d8bca0393
Javier Lagunes <mjavlagunes@gmail.com>
2024-08-03 18:06:34 -0600

    Changed the accordions in Members component for and expansion button

--- src/components/AboutUs/Members.jsx
+++ src/components/AboutUs/Members.jsx
@@ -32,151 +32,36 @@ const Members = () => {
         <h1 className='text-center text-warning border-bottom border-4 border-warning mb-4 col-3'>
           Integrantes
         </h1>
-        <div className='accordion' id='accordionExample'>
-          <div className='accordion-item bg-dark'>
-            <h2 className='accordion-header'>
-              <button
-                className='accordion-button'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#collapseOne'
-                aria-expanded='true'
-                aria-controls='collapseOne'
-              >
-                Líderes del equipo
-              </button>
-            </h2>
-            <div
-              id='collapseOne'
-              className='accordion-collapse collapse show'
-              data-bs-parent='#accordionExample'
-            >
-              <div className='accordion-body'>
-                <div className='row justify-content-center align-items-center'>
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='accordion-item bg-dark'>
-            <h2 className='accordion-header'>
-              <button
-                className='accordion-button collapsed'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#collapseTwo'
-                aria-expanded='false'
-                aria-controls='collapseTwo'
-              >
-                Aerodinámica
-              </button>
-            </h2>
-            <div
-              id='collapseTwo'
-              className='accordion-collapse collapse'
-              data-bs-parent='#accordionExample'
-            >
-              <div className='accordion-body'>
-                <div className='row justify-content-around'>
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='accordion-item bg-dark'>
-            <h2 className='accordion-header'>
-              <button
-                className='accordion-button collapsed'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#collapseThree'
-                aria-expanded='false'
-                aria-controls='collapseThree'
-              >
-                CDH
-              </button>
-            </h2>
-            <div
-              id='collapseThree'
-              className='accordion-collapse collapse'
-              data-bs-parent='#accordionExample'
-            >
-              <div className='accordion-body'>
-                <div className='row justify-content-around'>
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='accordion-item bg-dark'>
-            <h2 className='accordion-header'>
-              <button
-                className='accordion-button collapsed'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#collapseFour'
-                aria-expanded='false'
-                aria-controls='collapseFour'
-              >
-                EPS
-              </button>
-            </h2>
-            <div
-              id='collapseFour'
-              className='accordion-collapse collapse'
-              data-bs-parent='#accordionExample'
-            >
-              <div className='accordion-body'>
-                <div className='row justify-content-around'>
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='accordion-item bg-dark'>
-            <h2 className='accordion-header'>
-              <button
-                className='accordion-button collapsed'
-                type='button'
-                data-bs-toggle='collapse'
-                data-bs-target='#collapseFive'
-                aria-expanded='false'
-                aria-controls='collapseFive'
-              >
-                Mecánica
-              </button>
-            </h2>
-            <div
-              id='collapseFive'
-              className='accordion-collapse collapse'
-              data-bs-parent='#accordionExample'
-            >
-              <div className='accordion-body'>
-                <div className='row justify-content-around'>
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                  {membcard}
-                </div>
-              </div>
+        <div className='row justify-content-center align-items-center'>
+          {membcard}
+          {membcard}
+          {membcard}
+          {membcard}
+          {membcard}
+          {membcard}
+        </div>
+        <p class="d-inline-flex row justify-content-center align-items-center col-2">
+          <button class="btn btn-outline-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            Ver más integrantes
+          </button>
+        </p>
+        <div class="collapse" id="collapseExample">
+          <div class="card card-body bg-dark">
+            <div className='row justify-content-center align-items-center'>
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
+              {membcard}
             </div>
           </div>
         </div>
