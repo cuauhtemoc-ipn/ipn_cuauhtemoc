cd10e2d7878a48bc7da52cc896851f0763a764f6
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-08-12 01:10:55 -0600

    Solved navbar issue by commenting import of bootstrap js in main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 // Bootstrap CSS
 import '@/styles/main.scss'
 // Bootstrap Bundle JS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
