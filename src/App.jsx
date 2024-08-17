8ab53c6a3d6244221a1de7d2426e1d40f254d712
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-08-16 23:35:39 -0600

    Included Scrol to top when changing pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 import RoutesIndex from '@/routes'
 import Header from '@/components/Header/Header'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
 function App () {
   return (
     <>
       <BrowserRouter>
+        <ScrollToTop />
+
         <Header />
         <RoutesIndex />
         <Footer />
