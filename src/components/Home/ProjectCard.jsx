4d930742fe1100d56701d05c4feda1601948f00e
alainGonzalez-11 <alain.gonzalez.d@gmail.com>
2024-09-01 09:05:38 +0400

    Disabled netlify form

--- src/components/Home/ProjectCard.jsx
+++ src/components/Home/ProjectCard.jsx
@@ -5,16 +5,21 @@ const ProjectCard = ({ data }) => {
   return (
     <div className='card-container'>
       <div className='card-info'>
-        <img src={data.src} alt="" />
+        <img src={data.src} alt='' />
         <div className='text-light card-txt'>
           <h4 className='font-weight-bold h4 text-center'>{data.title}</h4>
           <p>
             {data.text.length > 255
-            ? data.text.slice(0, 255) + '...'
-            : data.text}
+              ? data.text.slice(0, 255) + '...'
+              : data.text}
           </p>
         </div>
-        <a href="#" className='text-warning font-weight-bold position-absolute h5 text-decoration-none'>Vew más...</a>
+        <a
+          href='#'
+          className='text-warning font-weight-bold position-absolute h5 text-decoration-none'
+        >
+          Vew más...
+        </a>
       </div>
     </div>
   )
