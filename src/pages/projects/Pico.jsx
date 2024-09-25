9461a4666ef2aca3bbec13f640190909be86ee9c
Javier Lagunes <mjavlagunes@gmail.com>
2024-09-25 01:51:22 -0600

    Added info for each competition, also added the new image of the mascot for the team, changed text fos sponsors

--- src/pages/projects/Pico.jsx
+++ src/pages/projects/Pico.jsx
@@ -16,8 +16,8 @@ const Pico = () => {
       />
       <ProjectDetailHeader
         image={projectHeader}
-        name='Picosatélites Puebla'
-        text='La Competencia Nacional de Picosatélites en Puebla, organizada por el Instituto Tecnológico de Puebla, permite a los estudiantes diseñar y lanzar pequeños satélites llamados CANSAT, simulando misiones espaciales reales. Los equipos deben desarrollar tecnologías que recojan datos de telemetría durante el vuelo y transmitan la información a una estación en tierra. Este evento fomenta el aprendizaje interdisciplinario en áreas como electrónica y aeronáutica, ofreciendo una experiencia completa en el ciclo de vida de un proyecto espacial, desde la planificación hasta el análisis post-lanzamiento, bajo los estándares de UNISEC México.'
+        name='Picosatélites'
+        text='La Competencia Nacional de Picosatélites, permite a los estudiantes diseñar y lanzar pequeños satélites llamados CANSAT, simulando misiones espaciales reales. Los equipos deben desarrollar tecnologías que recojan datos de telemetría durante el vuelo y transmitan la información a una estación en tierra. Este evento fomenta el aprendizaje interdisciplinario en áreas como electrónica y aeronáutica, ofreciendo una experiencia completa en el ciclo de vida de un proyecto espacial, desde la planificación hasta el análisis post-lanzamiento, bajo los estándares de UNISEC México.'
         url='https://www.puebla.tecnm.mx/concurso-nacional-pico-satelites-educativos-cansat/'
       />
       <Tabs data={content.data} />
