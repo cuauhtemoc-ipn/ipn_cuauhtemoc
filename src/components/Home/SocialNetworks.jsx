940b81a29f7f9859b8c26cdbb7a6fded19008d8c
Javier Lagunes <mjavlagunes@gmail.com>
2024-10-20 19:07:14 -0600

    Changed image in home page for social networks

--- src/components/Home/SocialNetworks.jsx
+++ src/components/Home/SocialNetworks.jsx
@@ -3,7 +3,6 @@ import insta from '@/assets/instalogo.png'
 import linkedin from '@/assets/linkedinlogo.png'
 import face from '@/assets/facebooklogo.svg'
 import Pollo from '@/assets/Pollomundo.png'
-import { NavLink } from 'react-router-dom'
 
 const Social = () => {
   return (
@@ -13,24 +12,54 @@ const Social = () => {
       </h2>
       <div className='row text-light justify-content-center align-items-center mx-0'>
         <div className=' col-6'>
-          <h3 className='text-light text-justify display-5 fw-semibold m-4'>Conecta con nuestras redes sociales para ver nuestras últimas publicaciones</h3>
+          <p className='lead-lg text-light text-justify fw-semibold'>
+            Siempre estamos haciendo algo emocionante, y queremos que seas parte
+            de ello. Síguenos en redes sociales para obtener actualizaciones
+            exclusivas, consejos y contenido que te inspirará.
+          </p>
+          <p className='lead-lg text-light text-justify fw-semibold'>
+            No te lo pierdas. Mantente al día con nuestras últimas noticias,
+            proyectos y contenido exclusivo siguiéndonos en redes sociales.
+            ¡Únete hoy y forma parte de nuestro camino!
+          </p>
           <div className='border-top border-bottom border-5 py-4 mx-4'>
-            <div className='row align-items-center py-2 mx-0 col-12'>
-              <img src={insta} className='row mx-auto col-2' />
+            <div className='row justify-content-center align-items-center py-2 mx-0 col-12'>
+              <a
+                href='https://www.instagram.com/cuauhtemocipn/'
+                target='_blank'
+                className='col-2'
+              >
+                <img src={insta} className='row mx-auto col-12' />
+              </a>
               <p className='lead-lg col-6'>cuauhtemocipn</p>
             </div>
-            <div className='row align-items-center py-2 mx-0 col-12'>
-              <img src={linkedin} className='row mx-auto col-2' />
+            <div className='row justify-content-center align-items-center py-2 mx-0 col-12'>
+              <a
+                href='https://www.linkedin.com/company/cuauht%C3%A9moc-ipn-aeroespacial/'
+                target='_blank'
+                className='col-2'
+              >
+                <img src={linkedin} className='row mx-auto col-12' />
+              </a>
               <p className='lead-lg col-6'>Cuauhtemoc IPN Aeroespacial</p>
             </div>
-            <div className='row align-items-center py-2 mx-0 col-12'>
-              <img src={face} className='row mx-auto col-2' />
+            <div className='row justify-content-center align-items-center py-2 mx-0 col-12'>
+              <a
+                href='https://www.facebook.com/CuauhtemocIPN'
+                target='_blank'
+                className='col-2'
+              >
+                <img src={face} className='row mx-auto col-12' />
+              </a>
               <p className='lead-lg col-6'>Cuauhtemoc IPN Aeroespacial</p>
             </div>
           </div>
         </div>
         <div className='col-6'>
-          <img src={Pollo} className='row mx-auto col-9' />
+          <img
+            src={Pollo}
+            className='row mx-auto col-9 col-lg-10 col-xl-9 col-xxl-7'
+          />
         </div>
       </div>
     </div>
