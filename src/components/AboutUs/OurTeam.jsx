1847ea0b395cf34f0951f89fad0ad904e2be4d4a
Javier Lagunes <mjavlagunes@gmail.com>
2025-02-12 20:57:56 -0600

    Video modified for OurTeam section

--- src/components/AboutUs/OurTeam.jsx
+++ src/components/AboutUs/OurTeam.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import info from '@/content/OurTeam.json'
+import camp from '@/assets/camp.mp4'
 
 const OurTeam = () => {
   const team = info.data[0]
@@ -14,15 +15,9 @@ const OurTeam = () => {
         </div>
         <div className='row justify-content-center align-items-center mx-0'>
           <div className='embed-responsive d-flex justify-content-center col-12 col-lg-7'>
-            <iframe
-              src={team.video[0]}
-              width='560'
-              height='315'
-              style={{ border: 'none', overflow: 'hidden' }}
-              allow='autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share'
-              allowFullScreen
-              title='Facebook video'
-            />
+            <video width='640' height='360' controls>
+              <source src={camp} type='video/mp4' />
+            </video>
           </div>
           <div className='lead-lg text-light text-justify my-sm-4 my-lg-0 col-11 col-lg-5'>
             <p>{team.description}</p>
